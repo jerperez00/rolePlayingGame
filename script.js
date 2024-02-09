bcaa04f8fc3fb9de583d57f3f334c36bc56b247f
jerperez00 <jesusdrp00@gmail.com>
2024-02-09 00:01:45 -0500

    Final

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const locations = [
       "Go to town square",
       "Go to town square",
     ],
-    "button functions": [goTown, goTown, goTown],
+    "button functions": [goTown, goTown, easterEgg],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
   },
   {
@@ -101,6 +101,12 @@ const locations = [
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;",
+  },
+  {
+    name: "easter egg",
+    "button text": ["2", "8", "Go to town square?"],
+    "button functions": [pickTwo, pickEight, goTown],
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!",
   },
 ];
 //initialize buttons
@@ -252,4 +258,30 @@ function isMonsterHit() {
 function easterEgg() {
   update(locations[7]);
 }
-function pick(guess) {}
+function pick(guess) {
+  const numbers = [];
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    numbers.push(Math.floor(Math.random() * 11));
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0){
+      lose();
+    }
+  }
+  
+}
+function pickTwo() {
+  pick(2);
+}
+function pickEight() {
+  pick(8);
+}
